4753c0c5c73da32445188f3896d873a40c1eb3fe
juhui88 <sand8594@hufs.ac.kr>
2023-06-24 11:10:06 +0900

    Fix Profile, Mypage

--- dontlate/src/route/Profile.js
+++ dontlate/src/route/Profile.js
@@ -3,6 +3,7 @@ import Bar, { Layout } from "../components/Bar";
 import Button from "../components/Button";
 import { useForm } from "react-hook-form";
 import tw from "tailwind-styled-components";
+import axios from "axios";
 
 const Span = tw.span`
 text-lg font-bold text-textColor"
@@ -14,7 +15,8 @@ shadow-inner
 const Profile = () => {
   const { register, handleSubmit, watch } = useForm();
   const [isPerfect, setIsPerfect] = useState(false);
-  const id = "sand8594@naver.com";
+  const userId = localStorage.getItem("id");
+  const [userData, setUserData] = useState();
 
   const onValid = (data) => {
     console.log(data);
@@ -24,12 +26,24 @@ const Profile = () => {
     console.log(e);
     setIsPerfect(true);
   };
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}users/detail/${userId}`)
+      .then((res) => {
+        setUserData(res.data.user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Layout>
       <Bar />
       <div className="px-16 py-10 space-y-7">
         <div className="mx-44 flex flex-col items-center space-y-4">
-          <img className="w-24" src={`img/profileImgs/${1}.png`} />
+          <img
+            className="w-24"
+            src={`img/profileImgs/${userData?.profile_img_num}.png`}
+          />
           <Button
             isPerfect={true}
             content={"프로필 사진 편집"}
@@ -41,11 +55,11 @@ const Profile = () => {
           className="border-[1px] border-mainColor rounded-md p-10 px-32 grid grid-cols-2 gap-8"
         >
           <Span>아이디</Span>
-          <Span>{id}</Span>
+          <Span>{userData?.email}</Span>
           <Span>비밀번호</Span>
           <Input
             {...register("pw", {
-              value: "123456789",
+              value: "wngml1234",
               minLength: {
                 value: 8,
                 message: "8자 이상 입력해주세요",
@@ -64,14 +78,14 @@ const Profile = () => {
           <Span>닉네임</Span>
           <Input
             {...register("nickName", {
-              value: "djfkls",
+              value: userData.name,
             })}
             onChange={handleInputChange}
           />
           <Span>전화번호</Span>
           <Input
             {...register("phoneNum", {
-              value: "ㅇㄹㄴㅇㄹ",
+              value: userData.phoneNumber,
               minLength: {
                 value: 11,
                 message: "휴대폰번호 11자를 입력해주세요",
